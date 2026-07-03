3b5eabff6b96f1510ab9d905615882787e8f80a9
Lucas Félix <felixworkspace7@gmail.com>
2026-07-03 19:12:16 -0300

    add controller

---
+++ src/main/java/dev/java10x/ninjasignup/Controller.java
@@ -0,0 +1,16 @@
+package dev.java10x.ninjasignup;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, API!";
+    }
+
+}
