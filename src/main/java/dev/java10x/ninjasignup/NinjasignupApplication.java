c9b3bfedf9413627466bd4ba74730dc90133e0c2
Lucas Félix <felixworkspace7@gmail.com>
2026-07-03 18:34:00 -0300

    first commit

---
+++ src/main/java/dev/java10x/ninjasignup/NinjasignupApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.ninjasignup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjasignupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjasignupApplication.class, args);
+	}
+
+}
